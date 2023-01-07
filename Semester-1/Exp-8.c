dd5cf024dfea225b415f81a07b5e4baa3ba65c2a
FoXDev-404 <114016588+FoXDev-404@users.noreply.github.com>
2023-01-07 20:12:36 +0530

    /*AIM:- Write a program that find whether a given number is even or odd.*/

---
+++ Semester-1/Exp-8.c
@@ -0,0 +1,23 @@
+/*AIM:- Write a program that find whether a given number is even or odd.*/
+
+// Author:- Rajpal Nishad FT-2 Roll No.-23
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    printf("Enter a: ");
+    scanf("%d", &a);
+
+    if (a % 2 == 0)
+    {
+        printf("The given number(%d) is EVEN", a);
+    }
+    else
+    {
+        printf("The given number(%d) is ODD", a);
+    }
+    return 0;
+}
