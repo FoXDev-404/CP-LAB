2d5acf1f10ac328e1bcc5cec9628ef8c6ee92247
FoXDev-404 <114016588+FoXDev-404@users.noreply.github.com>
2023-01-07 18:50:45 +0530

    /*AIM: Writing a program that accept the marks of 5 subjects and finds the sum and percentage marks obtaied*/

---
+++ Semester-1/Exp-1.c
@@ -0,0 +1,31 @@
+/*AIM: Writing a program that accept the marks of 5 subjects
+and finds the sum and percentage marks obtaied*/
+
+// Author: Rajpal Nishad FT-2 Roll No.-23
+
+#include <stdio.h>
+
+int main()
+{
+    float eng, phy, chem, math, comp;
+    float total, average, percentage;
+
+    printf("Enter marks of English :- ");
+    scanf("%f", &eng);
+    printf("Enter marks of Physics :- ");
+    scanf("%f", &phy);
+    printf("Enter marks of Chemistry :- ");
+    scanf("%f", &chem);
+    printf("Enter marks of Math :- ");
+    scanf("%f", &math);
+    printf("Enter marks of Computer :- ");
+    scanf("%f", &comp);
+
+    total = eng + phy + chem + math + comp;
+    percentage = (total / 500.0) * 100;
+
+    printf("Total marks = %f\n", total);
+    printf("Percentage = %f", percentage);
+
+    return 0;
+}
