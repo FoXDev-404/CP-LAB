bf768f4da87fdf7d7a57ed8edd464bc9cd686740
FoXDev-404 <114016588+FoXDev-404@users.noreply.github.com>
2023-01-26 16:18:06 +0530

    initial

---
+++ Semester-1/Temp/Raj.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+    for (i = 1; i <= 10; i)
+    {
+        printf("\t\t|\*\      /*/| \n");
+        printf("\t\t| \*\    /*/ | \n");
+        printf("\t\t|  \*\  /*/  | \n");
+        printf("\t\t|   \*\/*/   | \n");
+        printf("\t\t|    /*\    | \n");
+        printf("\t\t|   /*/\*\   | \n");
+        printf("\t\t|  /*/  \*\  | \n");
+        printf("\t\t| /*/    \*\ | \n");
+        printf("\t\t|/*/      \*\| \n");
+    }
+    return 0;
+}
